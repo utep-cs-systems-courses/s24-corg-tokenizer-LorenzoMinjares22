3ff38833b85b91e37f58966b7797528c5dc31e2b
LorenzoMinjares22 <lminjares3@miners.utep,edu>
2024-03-28 13:49:16 -0600

    user interface still working on it

--- src/uimain.c
+++ src/uimain.c
@@ -4,20 +4,20 @@
 #include "history.h"
  
 int main(){
- 
+  char input[100] 
   char *str;
   char **tokens = (char**)malloc(100*sizeof(char*));
   List *link_list = init_history();
   goto userInput;
     
  userInput:
-   char input[100];
+   
    printf("Input string input enter when done:");
    while(input[0] != '\n'){
      printf(">");
      fgets(input,sizeof(input),stdin);
      
-     if(input[0]) == '\n') break;
+     if(input[0] == '\n') break;
      str = input;
      tokens =tokenize(str);
      for(int i =0; i<count_tokens(str); i++){
@@ -31,27 +31,29 @@ int main(){
    printf("Get all history: h |  Get specific history: ! |  Done:  \n |  Input More: i");
  
    char his;
-   int id;
+   char id;
 
    his = getchar();
    while(his != '\n'){
      if(his == 'h'){
        print_history(link_list);
+       printf("Get all history: h |  Get specific history: ! |  Done:  \n |  Input More: i");
+       his = getchar();
     }
      if(his == '!'){
        printf("input ID of history");
        id = getchar();
        printf("history at ID: %d \t %s\n", id, get_history(link_list, id));
-
+       printf("Get all history: h |  Get specific history: ! |  Done:  \n |  Input More: i");
      }
 
      if(his == 'i'){
-       goto userinput;
+       goto userInput;
 
      }
-  
+   } 
 
-     printf("thank you !!!");
+printf("thank you !!!");
  
 
  
